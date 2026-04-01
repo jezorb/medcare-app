b121e36b4a41b5136b43a388366f522ca093c22a
jezorb <yatuljaat0212@gmail.com>
2026-04-01 15:46:00 +0530

    Delete client directory

--- client/src/api/appointments.js
+++
@@ -1,9 +0,0 @@
-import { api } from './client';
-
-export const createAppointment = (payload) => api.post('/appointment/create', payload).then((res) => res.data);
-export const updateAppointment = (appointmentId, payload) => api.put(`/appointment/update/${appointmentId}`, payload).then((res) => res.data);
-export const cancelAppointment = (appointmentId, payload) => api.patch(`/appointment/cancel/${appointmentId}`, payload).then((res) => res.data);
-export const addPrescription = (appointmentId, payload) => api.post(`/appointment/prescription/${appointmentId}`, payload).then((res) => res.data);
-export const getPatientAppointments = (patientId) => api.get(`/appointment/patient/${patientId}`).then((res) => res.data);
-export const getDoctorAppointments = (doctorId) => api.get(`/appointment/doctor/${doctorId}`).then((res) => res.data);
-export const getAppointment = (appointmentId) => api.get(`/appointment/${appointmentId}`).then((res) => res.data);
--- client/src/api/doctors.js
+++
@@ -1,9 +0,0 @@
-import { api } from './client';
-
-export const getDoctors = () => api.get('/doctor/doctors').then((res) => res.data);
-export const searchDoctors = (term) => api.get(`/doctor/search?term=${encodeURIComponent(term)}`).then((res) => res.data);
-export const getDoctorById = (doctorId) => api.get(`/doctor/${doctorId}`).then((res) => res.data);
-export const getDoctorReviews = (doctorId, params = '') => api.get(`/doctor/${doctorId}/reviews${params}`).then((res) => res.data);
-export const addDoctorReview = (doctorId, payload) => api.post(`/doctor/${doctorId}/reviews`, payload).then((res) => res.data);
-export const updateDoctor = (doctorId, payload) => api.patch(`/doctor/${doctorId}`, payload).then((res) => res.data);
-export const deleteDoctor = (doctorId) => api.delete(`/doctor/${doctorId}`).then((res) => res.data);
